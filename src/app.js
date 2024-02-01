94392c16d825e017e26569e2fa2ad1c0bb66416f
CWTWIS <chawanrat.wis@gmail.com>
2024-02-01 12:22:47 +0700

    config necessary middleware

---
+++ src/app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const limiter = require("./middleware/rate-limit");
+const error = require("./middleware/error");
+const notFound = require("./middleware/not-found");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(limiter);
+app.use(morgan("dev"));
+
+// app.use((req, res) => {
+//   throw new Error("test middleware error");
+// });
+// app.use((req, res) => res.json("sent leaw"));
+
+app.use(notFound);
+app.use(error);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`server is running on port: ${PORT}`));
